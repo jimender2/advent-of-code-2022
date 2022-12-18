ba285681e91a256f7263d37170f6c5c0ba6debfc
jimender2 <35303639+jimender2@users.noreply.github.com>
2022-12-17 22:08:57 -0500

    Solution for day 4

--- Day-4/main.go
+++ Day-4/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,13 +17,48 @@ func main() {
 
 	scanner := bufio.NewScanner(dat)
 
+	totalContains := 0
 	for scanner.Scan() {
 		tempText := scanner.Text()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(tempText)
+		// fmt.Println(tempText)
 
+		values := strings.Split(tempText, ",")
+
+		firstExpand := expandRange(values[0])
+		secondExpand := expandRange(values[1])
+
+		if strings.Contains(firstExpand, secondExpand) {
+			totalContains = totalContains + 1
+		} else if strings.Contains(secondExpand, firstExpand) {
+			totalContains = totalContains + 1
+		}
 	}
 
+	fmt.Println(totalContains)
+}
+
+func expandRange(str string) string {
+
+	values := strings.Split(str, "-")
+
+	// convert from string to int
+	firstInt, err := strconv.Atoi(values[0])
+	if err != nil {
+		panic(err)
+	}
+
+	secondInt, err := strconv.Atoi(values[1])
+	if err != nil {
+		panic(err)
+	}
+
+	str = ","
+	for i := firstInt; i <= secondInt; i++ {
+		str = str + strconv.Itoa(i) + ","
+	}
+
+	return str
 }
